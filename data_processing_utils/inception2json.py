c501dbe995eb498028cd72625b5bc02ecbe2ed19
Zulfat Miftahutdinov <zulfatmi@gmail.com>
2020-02-06 14:02:58 +0300

    update converter

--- data_processing_utils/inception2json.py
+++ data_processing_utils/inception2json.py
@@ -1,6 +1,8 @@
 from argparse import ArgumentParser
 import pandas as pd
+from glob import glob
 import json
+import os
 
 
 def read_texts(file_path):
@@ -8,40 +10,68 @@ def read_texts(file_path):
     with open(file_path, encoding='utf-8') as input_stream:
         for line in input_stream:
             if not line.startswith('#Text='): continue
-            texts.append(line[6:])
+            texts.append(line[6:].strip())
     return texts
+
+
+def get_text_from_spans(entity, sentences):
+    sentence = sentences[entity['sentence_id']]
+    return sentence[entity['start']:entity['end']]
+
+
+def get_entities_spans(annotation_data):
+    annotation_data['sentence_id'] = annotation_data.token_id.apply(lambda tid: int(tid.split('-')[0]))
+    annotation_data['entity_type'] = annotation_data.entity_type.apply(lambda eid: eid[:-3])
+    annotation_data['start'] = annotation_data.token_span.apply(lambda tid: int(tid.split('-')[0]))
+    annotation_data['end'] = annotation_data.token_span.apply(lambda tid: int(tid.split('-')[1]))
+    sentences_starts = annotation_data.groupby('sentence_id')['start'].min().reset_index().\
+        rename(columns={'start': 'sentence_start'})
+
+    annotation_data = annotation_data[annotation_data.entity_id != '_']
+    entities_starts = annotation_data.groupby(['sentence_id', 'entity_id', 'entity_type'])['start'].min().reset_index()
+    entities_ends = annotation_data.groupby(['sentence_id', 'entity_id', 'entity_type'])['end'].max().reset_index()
+
+    entities = pd.merge(entities_starts, entities_ends, on=['sentence_id', 'entity_id', 'entity_type'])
+    entities = pd.merge(entities, sentences_starts, on=['sentence_id'])
+    entities['start'] = entities['start'] - entities['sentence_start']
+    entities['end'] = entities['end'] - entities['sentence_start']
+    return entities
+
+
+def parse_sentences(file_path):
+    resulting_data = []
+    sentences = read_texts(file_path)
+    annotation_data = pd.read_csv(file_path, sep='\t', skip_blank_lines=True, comment='#', encoding='utf-8',
+                                  names=['token_id', 'token_span', 'token', 'entity_id', 'entity_type',
+                                         'unknown_1', 'unknown_2'])
+    entities_spans = get_entities_spans(annotation_data)
+    entities_spans['entity_text'] = entities_spans.apply(get_text_from_spans, args=(sentences,), axis=1)
+    for sentence_id, sentence in enumerate(sentences):
+        sentence_annotations = entities_spans[entities_spans.sentence_id == sentence_id]
+        sentence_annotations = sentence_annotations[['entity_id', 'entity_type', 'start', 'end', 'entity_text']]
+        resulting_data.append({
+            'text': sentence,
+            'sentence_id': sentence_id,
+            'file_name': os.path.basename(file_path),
+            'entities': sentence_annotations.to_dict(orient='records')
+        })
+    return resulting_data
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('--input_csv')
+    parser.add_argument('--input_folder')
     parser.add_argument('--save_to')
     args = parser.parse_args()
-    annotation_data = pd.read_csv(args.input_csv, sep='\t', skip_blank_lines=True, comment='#', encoding='utf-8',
-                                  names=['token_id', 'token_span', 'token', 'entity_id', 'entity_type',
-                                         'unknown_1', 'unknown_2'])
-    texts = read_texts(args.input_csv)
-    annotation_data = annotation_data[annotation_data.entity_id != '_']
-    annotation_data['text_id'] = annotation_data.token_id.apply(lambda tid: int(tid.split('-')[0]))
-    annotation_data['entity_type'] = annotation_data.entity_type.apply(lambda eid: eid[:-3])
-    annotation_data['start'] = annotation_data.token_span.apply(lambda tid: int(tid.split('-')[0]))
-    annotation_data['end'] = annotation_data.token_span.apply(lambda tid: int(tid.split('-')[1]))
-    entities_starts = annotation_data.groupby(['text_id', 'entity_id', 'entity_type'])['start'].min().reset_index()
-    entities_ends = annotation_data.groupby(['text_id', 'entity_id', 'entity_type'])['end'].max().reset_index()
-    entities = pd.merge(entities_starts, entities_ends, on=['text_id', 'entity_id', 'entity_type'])
-    entities['entity_text'] = entities.apply(lambda entity:
-                                              texts[entity['text_id']][entity['start']:entity['end']], axis=1)
+
     output_data = []
-    for text_id, text in enumerate(texts):
-        text_annotations = entities[entities.text_id == text_id]
-        text_annotations = text_annotations[['entity_id', 'entity_type', 'start', 'end', 'entity_text']]
-        output_data.append({
-            'text': text,
-            'entities': text_annotations.to_dict(orient='records')
-        })
+    file_pattern = os.path.join(args.input_folder, '*')
+    for file_path in glob(file_pattern):
+        sentences = parse_sentences(file_path)
+        output_data += sentences
 
     with open(args.save_to, 'w', encoding='utf-8') as output_stream:
         for document in output_data:
-            serialized_data = json.dumps(document)
+            serialized_data = json.dumps(document, ensure_ascii=False)
             output_stream.write(serialized_data + '\n')
 
